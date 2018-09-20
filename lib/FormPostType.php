10270d0a7d97e720096f92c7055be44dc07b407e
Linsey Brander <linsey@bureaubright.nl>
2018-09-20 11:33:06 +0200

    Add ID column to form

--- lib/FormPostType.php
+++ lib/FormPostType.php
@@ -23,6 +23,19 @@ class FormPostType {
     );
     PostType::make( 'form', 'Formulieren', 'Formulier' )->set( $args );
 
+    add_action( 'manage_form_posts_columns', array( $this, 'set_custom_edit_form_columns' ) );
+    add_action( 'manage_form_posts_custom_column', array( $this, 'custom_form_column' ), 10, 2 );
+  }
+
+  public function set_custom_edit_form_columns($columns) {
+    $columns['form_id'] = 'ID';
+    return $columns;
+  }
+
+  public function custom_form_column($column, $post_id) {
+    if ( $column == 'form_id') {
+      echo $post_id;
+    }
   }
 
 }
